ab7611775994f01940d403b3190b8a89840af176
JavaSaurusStudios <98191221+JavaSaurusStudios@users.noreply.github.com>
2023-07-29 16:53:41 +0200

    Update twitch-utils.js

--- twitch/twitch-utils.js
+++ twitch/twitch-utils.js
@@ -35,18 +35,6 @@ function ActivateFunctions() {
 }
 
 function fireKey(key) {
-    SimulateKeyDown(key);
-    const myTimeout = setTimeout(SimulateKeyUp(key), 125);
+    keyDown(key);
+    const myTimeout = setTimeout(keyUp(key), 125);
 } 
-
-function SimulateKeyDown(key){
-    var evt_down = new KeyboardEvent('keydown', {'keyCode':key, 'which':key}); 
-    document.dispatchEvent (evt_down); 
-}
-
-function SimulateKeyUp(key){
-    var evt_up = new KeyboardEvent('keyup', {'keyCode':key, 'which':key}); 
-    document.dispatchEvent (evt_up); 
-}
-
-
