cf0ff33e319d8b12f50412b957d473607b3b75e7
JavaSaurusStudios <98191221+JavaSaurusStudios@users.noreply.github.com>
2023-07-29 16:07:00 +0200

    Update IodineGBACoreGlueCode.js

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -243,7 +243,7 @@ function registerGUIEvents() {
     addEvent("keyup", document, keyUpPreprocess);
     addEvent("unload", window, ExportSave);
     Iodine.attachSpeedHandler(function (speed) {
-        document.title = games[location.hash.substr(1)] + " - " + speed;
+        document.title = games[GAME] + " - " + speed;
     });
 }
 function lowerVolume() {
